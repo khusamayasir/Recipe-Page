b5a95c611877e79b18d90da886b6342bf877c4b9
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-07-05 22:36:24 +0500

    update main.js

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ prep.classList.add("flex");
 
 let cook = document.querySelector(".cook-time");
 cook.classList.add("flex");
+
+let total = document.querySelector(".total-time");
+total.classList.add("flex");
