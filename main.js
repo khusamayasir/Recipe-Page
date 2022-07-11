b0d46641202f51c1e63d6e3701c1f3f658173aca
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-07-11 18:16:49 +0500

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+let p = document.getElementsByTagName("p");
+p[1].style.color = "orange";
+p[0].style.color = "#BDBDBD";
+p[2].style.color = "#BDBDBD";
+p[3].style.color = "#BDBDBD";
+p[4].style.color = "#BDBDBD";
+p[7].style.color = "#BDBDBD";
+p[7].style.marginBlockStart = "50px";
+p[7].style.marginBlockEnd = "60px";
+p[0].style.fontSize = "12px";
+p[2].style.fontSize = "12px";
+p[3].style.fontSize = "12px";
+p[4].style.fontSize = "12px";
+
 let dining = document.querySelector(".yield");
 dining.classList.add("flex");
 
